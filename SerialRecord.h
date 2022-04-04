8fdd48e6f2d7bc4bd6c8e5f7d09b49c7ecebb14b
Oliver Steele <steele@osteele.com>
2022-04-04 11:00:47 +0800

    Switch to Google formatting; fix a bug

--- SerialRecord.h
+++ SerialRecord.h
@@ -1,141 +1,117 @@
-class SerialValueReader
-{
-public:
-  SerialValueReader(int count = 1) : size(count), values(new int[count]), m_buffer(new int[count]) {}
+class SerialValueReader {
+ public:
   const int size;
   int *values;
 
-  const int get(int index)
-  {
-    if (0 <= index && index < size)
-    {
+  SerialValueReader(int count = 1)
+      : size(count), values(new int[count]), m_buffer(new int[count]) {}
+
+  const int get(int index) {
+    if (0 <= index && index < size) {
       return values[index];
-    }
-    else
-    {
+    } else {
       Serial.println("Error: SerialValueReader.get index out of bounds");
       return -1;
     }
   }
 
-  int set(int index, int value)
-  {
-    if (0 <= index && index < size)
-    {
+  int set(int index, int value) {
+    if (0 <= index && index < size) {
       values[index] = value;
-    }
-    else
-    {
+    } else {
       Serial.println("Error: SerialValueReader.set index out of bounds");
     }
   }
 
-  const int operator[](int index)
-  {
-    return get(index);
-  }
-
-  // int operator[] =(int index, int value)
-  // {
-  //   return get(index);
-  // }
-
-  // int value()
-  // {
-  //   return this->get(0);
-  // }
+  const int operator[](int index) { return get(index); }
 
   // receive serial data from Processing
-  void getSerialData()
-  {
-    while (Serial.available())
-    {
+  void getSerialData() {
+    while (Serial.available()) {
       char c = Serial.read();
-      if (c == '\n')
-      {
-        switch (m_readState)
-        {
-        case IN_FIELD:
-          values[m_ix++] = m_accum;
-          if (m_ix < size)
-          {
-            Serial.println("Error: SerialValueReader received too few values");
-          }
-          break;
+      if (c == '\n') {
+        switch (m_readState) {
+          case IN_FIELD:
+            values[m_ix++] = m_accum;
+          // fall through
+          case FIELD_START:
+            if (m_ix < size) {
+              Serial.print(
+                  "Error: SerialValueReader received too few values: ");
+              Serial.print(m_ix);
+              Serial.print(" instead of ");
+              Serial.println(size);
+            }
+            break;
         }
         m_readState = LINE_START;
-      }
-      else
-      {
-        switch (m_readState)
-        {
-        case LINE_START:
-          if (c == '!') // commands
-          {
-            m_readState = COMMAND;
-            break;
-          }
-          m_readState = FIELD_START;
-          m_ix = 0;
-          // fall through to start field
-        case FIELD_START:
-          if (m_ix > size)
-          {
-            Serial.println("Error: SerialValueReader received too many values");
-            break;
-          }
-          m_accum = 0;
-          // fall through to in field
-        case IN_FIELD:
-          switch (c)
-          {
-          case '0' ... '9':
-            m_accum *= 10;
-            m_accum += c - '0';
-            // TODO: warn on overflow
-            break;
-          case ' ':
-          case ',': // end of field
-            values[m_ix++] = m_accum;
+      } else {
+        switch (m_readState) {
+          case LINE_START:
+            if (c == '!')  // commands
+            {
+              m_readState = COMMAND;
+              break;
+            }
             m_readState = FIELD_START;
+            m_ix = 0;
+            // fall through
+          case FIELD_START:
+            if (m_ix >= size) {
+              Serial.println(
+                  "Error: SerialValueReader received too many values");
+              m_readState = SKIP_LINE;
+              break;
+            }
+            m_readState = IN_FIELD;
+            m_accum = 0;
+            // fall through
+          case IN_FIELD:
+            switch (c) {
+              case '0' ... '9':
+                m_accum *= 10;
+                m_accum += c - '0';
+                // TODO: warn on overflow
+                break;
+              case ' ':
+              case ',':  // end of field
+                values[m_ix++] = m_accum;
+                m_readState = FIELD_START;
+                break;
+              default:
+                reportInvalidCharacter(
+                    "Error: SerialValueReader received invalid character: ", c);
+                m_readState = SKIP_LINE;
+                break;
+            }
             break;
-          default:
-            reportInvalidCharacter("Error: SerialValueReader received invalid character: ", c);
+          case COMMAND:
+            switch (c) {
+              case 'e':
+                send();
+                break;
+              default:
+                reportInvalidCharacter(
+                    "Error: SerialValueReader received unknown command: ", c);
+            }
             m_readState = SKIP_LINE;
             break;
-          }
-          break;
-        case COMMAND:
-          switch (c)
-          {
-          case 'e':
-            send();
-            break;
-          default:
-            reportInvalidCharacter("Error: SerialValueReader received unknown command: ", c);
-          }
-          m_readState = SKIP_LINE;
-          break;
         }
       }
     }
   }
-  void reportInvalidCharacter(char *message, char c)
-  {
+  void reportInvalidCharacter(char *message, char c) {
     Serial.print(message);
+    Serial.print('\'');
     Serial.print(c);
-    Serial.print(" (");
+    Serial.print("' (");
     Serial.print(static_cast<unsigned char>(c));
     Serial.println(")");
   }
 
-  void
-  send()
-  {
-    for (int i = 0; i < size; i++)
-    {
-      if (i > 0)
-      {
+  void send() {
+    for (int i = 0; i < size; i++) {
+      if (i > 0) {
         Serial.print(',');
       }
       Serial.print(values[i]);
@@ -143,15 +119,13 @@ public:
     Serial.println();
   }
 
-private:
+ private:
   int m_ix = 0;
   int m_accum = 0;
   int *m_buffer;
 
-  enum ReadState
-  {
+  enum ReadState {
     LINE_START,
-    // RECORD,
     FIELD_START,
     IN_FIELD,
     COMMAND,
