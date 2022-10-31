b932dc9baf9a4b3b6a25d9bcc77df58d2e969bb9
Oliver Steele <steele@osteele.com>
2022-10-31 18:52:10 +0800

    Publish zip before project, for better intermediate state in case of failure

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -19,5 +19,5 @@ zip_key=${S3_DIR}/${name}.zip
 aws s3 cp "$project_file" s3://${S3_BUCKET}/${project_file_key}
 aws s3 cp "$zip_file" s3://${S3_BUCKET}/${zip_key}
 
-aws s3api put-object-acl --bucket $S3_BUCKET --key $project_file_key --acl public-read
 aws s3api put-object-acl --bucket $S3_BUCKET --key $zip_key --acl public-read
+aws s3api put-object-acl --bucket $S3_BUCKET --key $project_file_key --acl public-read
